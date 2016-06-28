4e6e13987328d3dccbba7731f1187a1c84e8d0c2
Jeremy M. Reed <reeje76@gmail.com>
2016-06-28 11:11:20 -0600

    Stub out Triangle methods.

---
+++ src/main/java/triangle/Triangle.java
@@ -0,0 +1,30 @@
+package triangle;
+
+import exceptions.NotImplementedException;
+
+public class Triangle
+{	
+	/**
+	 * Integer objects should be non-null, and positive.
+	 * @param v1
+	 * @param v2
+	 * @param v3
+	 */
+	public Triangle(Integer side1, Integer side2, Integer side3 ) {
+		throw new NotImplementedException();
+	}
+	
+	/**
+	 * Double objects should be non-null, and positive.
+	 * @param v1
+	 * @param v2
+	 * @param v3
+	 */
+	public Triangle(Double side1, Double side2, Double side3 ) {
+		throw new NotImplementedException();
+	}
+	
+	public TriangleKind getKind() {
+		throw new NotImplementedException();
+	}
+}
